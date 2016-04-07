06c57ecca51c5856da588060ec4005cfe19c58c3
Zhou Peng <zhou.pen9@gmail.com>
2016-04-07 18:12:28 +0800

    Change files for deploy/install.
    Add README
    Delete config.sh
    Remove dev profile

--- config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp config/application.properties jar/
-cp config/application-derby.properties jar/
-
-
--- startup.sh
+++ startup.sh
@@ -1,8 +1,8 @@
-VA_HOME=/opt/jvm/current
+JAVA_HOME=/opt/jvm/current
 JAVA=$JAVA_HOME/bin/java
-PROGRAM=/opt/xpws/xpws-1.0.jar
-LOG_FILE=/opt/xpws/xpws.log
+#PROGRAM=/opt/xpws/xpws-1.0.jar
+LOG_FILE=/opt/xpws/logs/xpws.log
 
 #$JAVA -Djava.security.egd=/dev/urandom -jar $PROGRAM >> $LOG_FILE 2>&1 &
-$JAVA -Djava.security.egd=/dev/urandom -Djava.ext.dirs=$JAVA_HOME/jre/lib/ext:lib -cp jar cn.com.xinli.portal.PortalApplication >> $LOG_FILE 2>&1 &
+nohup $JAVA -Djava.security.egd=/dev/urandom -Djava.ext.dirs=$JAVA_HOME/jre/lib/ext:lib -cp jar cn.com.xinli.portal.PortalApplication >> $LOG_FILE 2>&1 &
 
